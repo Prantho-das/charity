05da9c0d637ddf7f662dab34cbc51a4d5c971c35
Prantho-das <pranthokumardas786@gmail.com>
2022-07-31 02:25:15 +0600

    bkash working

--- resources/views/frontend/partial/footer.blade.php
+++ resources/views/frontend/partial/footer.blade.php
@@ -85,13 +85,12 @@
                                         </div>
 
                                         <div class="form-group">
-                                            <input type="email" name="email" class="form-control" placeholder="E-mail"
-                                                required>
+                                            <input type="email" name="email" class="form-control"
+                                                placeholder="E-mail" required>
                                         </div>
 
                                         <div class="form-group">
-                                            <textarea name="message" class="form-control" placeholder="Message"
-                                                required></textarea>
+                                            <textarea name="message" class="form-control" placeholder="Message" required></textarea>
                                         </div>
 
                                         <div class="form-group alerts">
@@ -176,10 +175,11 @@
 ================================================== -->
 
 <!-- jQuery -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script id="myScript" src="https://scripts.sandbox.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout-sandbox.js">
+</script>{{-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> --}}
 <script>
-    window.jQuery || document.write('<script src="{{ asset('assets') }}/js/jquery-1.11.1.min.js"><\/script>')
-
+    window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.3.1.min.js"><\/script>')
 </script>
 
 <!-- Bootsrap javascript file -->
@@ -191,6 +191,109 @@
 <!-- Template main javascript -->
 <script src="{{ asset('assets') }}/js/main.js"></script>
 
+<script type="text/javascript">
+    alert("Right after init ");
+    var accessToken = '';
+    $(document).ready(function() {
+        $.ajax({
+            url: "token.php",
+            type: 'POST',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log('got data from token  ..');
+                console.log(JSON.stringify(data));
+
+                accessToken = JSON.stringify(data);
+            },
+            error: function() {
+                console.log('error');
+
+            }
+        });
+        var paymentConfig = {
+            createCheckoutURL: "createpayment.php",
+            executeCheckoutURL: "executepayment.php",
+        };
+
+        var paymentRequest;
+        paymentRequest = {
+            amount: '105',
+            intent: 'sale'
+        };
+        console.log(JSON.stringify(paymentRequest));
+        bKash.init({
+            paymentMode: 'checkout',
+            paymentRequest: paymentRequest,
+            createRequest: function(request) {
+                console.log('=> createRequest (request) :: ');
+                console.log(request);
+
+                $.ajax({
+                    url: paymentConfig.createCheckoutURL + "?amount=" + paymentRequest
+                        .amount,
+                    type: 'GET',
+                    contentType: 'application/json',
+                    success: function(data) {
+                        console.log('got data from create  ..');
+                        console.log('data ::=>');
+                        console.log(JSON.stringify(data));
+
+                        var obj = JSON.parse(data);
+
+                        if (data && obj.paymentID != null) {
+                            paymentID = obj.paymentID;
+                            bKash.create().onSuccess(obj);
+                        } else {
+                            console.log('error');
+                            bKash.create().onError();
+                        }
+                    },
+                    error: function() {
+                        console.log('error');
+                        bKash.create().onError();
+                    }
+                });
+            },
+
+            executeRequestOnAuthorization: function() {
+                console.log('=> executeRequestOnAuthorization');
+                $.ajax({
+                    url: paymentConfig.executeCheckoutURL + "?paymentID=" + paymentID,
+                    type: 'GET',
+                    contentType: 'application/json',
+                    success: function(data) {
+                        console.log('got data from execute  ..');
+                        console.log('data ::=>');
+                        console.log(JSON.stringify(data));
+
+                        data = JSON.parse(data);
+                        if (data && data.paymentID != null) {
+                            alert('[SUCCESS] data : ' + JSON.stringify(data));
+                            window.location.href = "success.html";
+                        } else {
+                            bKash.execute().onError();
+                        }
+                    },
+                    error: function() {
+                        bKash.execute().onError();
+                    }
+                });
+            }
+        });
+
+
+
+
+        function callReconfigure(val) {
+            bKash.reconfigure(val);
+        }
+
+        function clickPayButton() {
+            $("#bKash_button").trigger('click');
+        }
+        clickPayButton()
+    });
+</script>
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
     (function(b, o, i, l, e, r) {
@@ -207,7 +310,6 @@
     }(window, document, 'script', 'ga'));
     ga('create', 'UA-XXXXX-X');
     ga('send', 'pageview');
-
 </script>
 
 </body>
--- resources/views/frontend/partial/header.blade.php
+++ resources/views/frontend/partial/header.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Charity Management System </title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
